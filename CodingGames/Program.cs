d3c14ad3a8352649415c15930810facf6db3c9b3
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-11-29 17:14:17 -0500

    posted one more tweet

--- CodingGames/Program.cs
+++ CodingGames/Program.cs
@@ -1,2 +1,4 @@
-﻿using System.Linq;
-System.Linq.Enumerable.Range(0, 10).ToList().ForEach(i => { System.Console.Write(i); });
+﻿using System;
+using System.Linq;
+System.Linq.Enumerable.Range(0, 10).ToList().ForEach(i => { System.Console.WriteLine(i); });
+Console.WriteLine('a'+'b');
